931350611ce21261be0a5ae4e6f5d800d4259015
jlbbooks <dev@jlbbooks.it>
2017-04-18 17:16:35 +0200

    fix

--- application/libraries/EntryManager.php
+++ application/libraries/EntryManager.php
@@ -170,7 +170,7 @@ class EntryManager
 
     $existing_tags = array();
     
-    $tags_link = $this->_CI->mongo_db->select(array('_id'))->where(array('related.type' => 'proposal', 'related.id' => $id)->get('entry'); 
+    $tags_link = $this->_CI->mongo_db->where(array('related.type' => 'proposal', 'related.id' => $id))->get('entry'); 
           
     if(!empty($tags_link))
     {
