14882fdd6881f30a4259b1a3467ccb2e15da0696
jlbbooks <jlbbooks@users.noreply.github.com>
2017-04-19 16:11:52 +0200

    FIX

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -163,8 +163,7 @@ class Api extends REST_Controller
     }
 
     $entry=json_decode($put,TRUE);	 
-	  
-	  file_put_contents('debug.log',print_r($entry,TRUE),FILE_APPEND);
+    file_put_contents('debug.log',print_r($entry,TRUE),FILE_APPEND);
   
     if(!isset($entry['id']))
     {
