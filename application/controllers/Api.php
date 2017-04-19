d26dfe7d0122921ede4901a44e9f0212e15c1f94
jlbbooks <jlbbooks@users.noreply.github.com>
2017-04-19 10:28:26 +0200

    fix

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -358,7 +358,8 @@ class Api extends REST_Controller
       }
       $entry_data['metadata'] = $metadatas;
     }
-
+	  file_put_contents('debug.log','put','FILE_APPEND');
+    file_put_contents('debug.log',print_r($entry_data,TRUE),'FILE_APPEND');
     $response = $this->entrymanager->update($entry_data);  
     if(!empty($response)) $this->response(array('status' => true, 'data' => $response), REST_Controller::HTTP_OK);    
     else $this->response(array('status' => true, array()), REST_Controller::HTTP_OK);
@@ -593,7 +594,9 @@ class Api extends REST_Controller
       $this->response(array('true', array('message' => $error_code)), REST_Controller::HTTP_OK);
     }
     else
-    {
+    { 
+     file_put_contents('debug.log','post','FILE_APPEND');
+     file_put_contents('debug.log',print_r($entry_data,TRUE),'FILE_APPEND');
       $response = $this->entrymanager->save($entry_data);
       if(!empty($response)) $this->response(array('status' => true, 'data' => $response), REST_Controller::HTTP_OK);
       else $this->response(array('true', array()), REST_Controller::HTTP_OK);
