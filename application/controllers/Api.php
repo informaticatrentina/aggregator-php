3e35c45f23a184fc5f0c04a15ec543b62603b6fb
jlbbooks <jlbbooks@users.noreply.github.com>
2017-05-04 18:33:06 +0200

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -611,7 +611,7 @@ class Api extends REST_Controller
       $response = $this->entrymanager->save($entry_data);
       file_put_contents('debug.log','RESPONSE POST',FILE_APPEND); 
       file_put_contents('debug.log',print_r($response,TRUE),FILE_APPEND); 
-      if(!empty($response)) $this->response(array('status' => "true", 'id' => $response), REST_Controller::HTTP_OK);
+      if(!empty($response)) $this->response(array('status' => "true", 'id' => (string)$response), REST_Controller::HTTP_OK);
       else $this->response(array('true', array('miaoooo')), REST_Controller::HTTP_OK);
     }   
   }
