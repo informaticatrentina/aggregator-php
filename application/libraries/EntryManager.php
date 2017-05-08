dcf6fbe82df2e85993907f39154b9932568af8cf
jlbbooks <jlbbooks@users.noreply.github.com>
2017-05-08 16:09:02 +0200

    Update EntryManager.php

--- application/libraries/EntryManager.php
+++ application/libraries/EntryManager.php
@@ -841,10 +841,15 @@ class EntryManager
             {
               foreach($return_fields as $return_field)
               {               
-                file_put_contents('get',print_r($return_field,TRUE),FILE_APPEND);
                 $return_field=trim($return_field);                
                 if(isset($tmp_entry[$return_field]))
                 {
+                  // Formatto la data
+                  if($tmp_entry[$return_field]=='creation_date') 
+                  {
+                    file_put_contents('get',print_r($tmp_entry[$return_field],TRUE),FILE_APPEND);                    
+                    //$tmp_entry[$return_field]=date('Y-m-d H:i:s',);
+                  }
                   $outputEntry[$return_field] = $tmp_entry[$return_field];
                 }
               }
