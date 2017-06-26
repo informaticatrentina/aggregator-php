a3174c992e52157c1d094588a8d760384c44dcd3
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-06-26 17:01:09 +0200

    Fix

--- application/libraries/EntryManager.php
+++ application/libraries/EntryManager.php
@@ -726,9 +726,7 @@ $conditions['status'] = $user_data['status'];
         if(!is_array($sort))
         {
           // debudg
-           //$data=$this->_CI->mongo_db->where($conditions)->limit($limit)->offset($offset)->get('entry');
-           $data=$this->_CI->mongo_db->get('entry');
-           file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);
+           $data=$this->_CI->mongo_db->where($conditions)->limit($limit)->offset($offset)->get('entry');           
            $count = intval($this->_CI->mongo_db->where($conditions)->limit($limit)->offset($offset)->count('entry'));
         }
         else
